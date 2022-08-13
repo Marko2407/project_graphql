6256ea816b2ae60f67af0d9030e8862d0b9fdc2c
marko.vukosav <marko.vukosav@rimac-automobili.com>
2022-08-13 20:22:02 +0200

    test heroku deploy

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
-const express = require('express')
-const{ ApolloServer, gql } = require('apollo-server-express')
-const typeDefs = require('./typeDefs') 
-const workoutResolvers = require('./resolvers/workoutsResolver')
-const userResolvers = require('./resolvers/userResolver')
-const activitiesResolvers = require('./resolvers/activityResolver')
-const mongoose = require('mongoose')
-
-async function startServer(){
-    const app = express()
-    const apolloServer = new ApolloServer({
-        typeDefs: typeDefs,
-        resolvers: [workoutResolvers, userResolvers, activitiesResolvers],
-    });
-
-    await apolloServer.start()
-
-    apolloServer.applyMiddleware({app: app})
-
-    app.use((req, res) => {
-        res.send("Hello from express appolo server")
-    })
-
-    await mongoose.connect('mongodb://localhost:27017/mb_demo_db',{
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
-
-    console.log('Mongoose connected...')
-
-    app.listen(4000,() => console.log('Sever is running on port 4000'))
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-startServer()
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const workoutResolvers = require("./resolvers/workoutsResolver");
+const userResolvers = require("./resolvers/userResolver");
+const activitiesResolvers = require("./resolvers/activityResolver");
+const mongoose = require("mongoose");
+
+async function startServer() {
+  const app = express();
+  const apolloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: [workoutResolvers, userResolvers, activitiesResolvers],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app: app });
+
+  app.use((req, res) => {
+    res.send("Hello from express appolo server");
+  });
+
+  await mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+  console.log("Mongoose connected...");
+
+  let port = process.env.PORT || 4000;
+
+  app.listen(port, () => console.log("Sever is running " + port));
+}
+
+startServer();
--- zavrsni.js
+++
@@ -1,3 +0,0 @@
-const h1 = document.createElement('header');
-const root_u_sjeni = h1.attachShadow({ mode: 'open' });
-root_u_sjeni.innerHTML = '<h1> hello world! ';

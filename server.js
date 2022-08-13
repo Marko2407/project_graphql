1454ad9bc2f575b6065e98d1bd18b6d8ed527c1c
marko.vukosav <marko.vukosav@rimac-automobili.com>
2022-08-13 21:52:10 +0200

    change url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ async function startServer() {
     res.send("Hello from express appolo server");
   });
 
-  await mongoose.connect(process.env.DATABASE_URL, {
+  await mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });

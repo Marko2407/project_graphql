2250c5158b1c749e4d22a857e97953df79261894
marko.vukosav <marko.vukosav@rimac-automobili.com>
2022-08-20 18:12:13 +0200

    f

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 MONGODB_URI =
-  'mongodb+srv://user:user@mbdemo.krcxe.mongodb.net/?retryWrites=true&w=majority';
-DATABASE_URL = 'mongodb://localhost:27017/mb_demo_db';
+  "mongodb+srv://user:user@mbdemo.krcxe.mongodb.net/?retryWrites=true&w=majority";
+DATABASE_URL = "mongodb://localhost:27017/mb_demo_db";
 
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const typeDefs = require('./typeDefs');
-const workoutResolvers = require('./resolvers/workoutsResolver');
-const userResolvers = require('./resolvers/userResolver');
-const activitiesResolvers = require('./resolvers/activityResolver');
-const mongoose = require('mongoose');
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const workoutResolvers = require("./resolvers/workoutsResolver");
+const userResolvers = require("./resolvers/userResolver");
+const activitiesResolvers = require("./resolvers/activityResolver");
+const mongoose = require("mongoose");
 
 async function startServer() {
   const app = express();
@@ -26,19 +26,19 @@ async function startServer() {
   apolloServer.applyMiddleware({ app: app });
 
   app.use((req, res) => {
-    res.send('Hello from express appolo server');
+    res.send("Hello from express appolo server");
   });
 
-  await mongoose.connect(DATABASE_URL, {
+  await mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
 
-  console.log('Mongoose connected...');
+  console.log("Mongoose connected...");
 
   let port = process.env.PORT || 4000;
 
-  app.listen(port, () => console.log('Server is running ' + port));
+  app.listen(port, () => console.log("Server is running " + port));
 }
 
 startServer();
